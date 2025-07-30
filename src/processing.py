f350f1b528ca4f3bf13ec35f558f3938ff6f4246
Fox-IK <ilya-kim-1999@mail.ru>
2025-07-30 17:05:12 +0300

    init sort_by_date

--- src/processing.py
+++ src/processing.py
@@ -1,5 +1,21 @@
 def filter_by_state(data: list[dict], state_value: str = "EXECUTED") -> list[dict]:
     """
     Фильтрует список словарей по значению ключа 'state'
+
+    :param data: Список словарей для обработки
+    :param state_value: Значение для фильтрации (по умолчанию 'EXECUTED')
+    :return: Отфильтрованный список словарей
     """
     return [ item for item in data if item.get("state") == state_value ]
+
+
+def sort_by_date ( data: list [ dict ],reverse: bool = True ) -> list [ dict ] :
+    """
+    Сортирует список словарей по ключу 'date'
+
+    :param data: Список словарей для сортировки
+    :param reverse: Порядок сортировки (True - по убыванию, False - по возрастанию)
+    :return: Отсортированный список словарей
+    """
+    return sorted(data,key=lambda x : x [ "date" ],reverse=reverse)
+
